2019f6a56a39eda10bf430c91d44cb0fc8a851e6
Wilson Tomas <wilsonpumm@gmail.com>
2020-08-13 19:32:10 -0400

    Se agrego dapper a la clase de imagenes

--- Proyecto1/LibreriaDeClases/Interfases/IOpcionesImagenes.cs
+++ Proyecto1/LibreriaDeClases/Interfases/IOpcionesImagenes.cs
@@ -5,7 +5,6 @@ namespace LibreriaDeClases
     public interface IOpcionesImagenes
     {
         List<byte[]> ObtenerImagenesPorId(int Id);
-        List<Imagenes> ObtenerImagenesPorIdWF(int Id);
         void VaciarImagenesDeProducto(int Id);
 
         void InsertarImagenes(int Id, List<byte[]> imagenes);
--- Proyecto1/LibreriaDeClases/OpcionesImagenes.cs
+++ Proyecto1/LibreriaDeClases/OpcionesImagenes.cs
@@ -10,53 +10,24 @@ namespace LibreriaDeClases
     public class OpcionesImagenes : IOpcionesImagenes
     {
         public BaseDeDatos BaseDeDatos = new BaseDeDatos();
-        public List<Imagenes> ObtenerImagenesPorIdWF(int Id)
-        {
-            List<Imagenes> imagenes = new List<Imagenes>();
-            SqlCommand Comand = BaseDeDatos.Conection.CreateCommand();
-            Comand.CommandText = "SELECT * FROM ImagenesProducto WHERE IdProducto=@Id";
-            Comand.Parameters.AddWithValue("@Id", Id);
-            BaseDeDatos.Conection.Open();
-            SqlDataReader reader = Comand.ExecuteReader();
-            while (reader.Read())
-            {
-                Imagenes img = new Imagenes();
-                //img.IdProducto = (int)reader["IdProducto"];
-                // img.IdImagenes = (int)reader["IdImagenes"];
-                img.Imagen = (byte[])reader["Imagen"];
-                imagenes.Add(img);
-
-            }
-            BaseDeDatos.Conection.Close();
-
-            return imagenes;
-        }
 
         public void VaciarImagenesDeProducto(int Id)
-        {
-            SqlCommand comand = BaseDeDatos.Conection.CreateCommand();
-            comand.CommandText = "DELETE FROM ImagenesProducto WHERE IdProducto=@Id";
-            comand.Parameters.AddWithValue("@Id", Id);
-            BaseDeDatos.Conection.Open();
-            comand.ExecuteNonQuery();
-            BaseDeDatos.Conection.Close();
+        {         
+            string sql = "DELETE FROM ImagenesProducto WHERE IdProducto=@IdProducto";
+            BaseDeDatos.Conection.Execute(sql, new { @IdProducto=Id });
         }
 
         public List<byte[]> ObtenerImagenesPorId(int Id)
         {
+     
             List<byte[]> ListaDeImagenes = new List<byte[]>();
-            SqlCommand Comand = BaseDeDatos.Conection.CreateCommand();
-            Comand.CommandText = "SELECT * FROM ImagenesProducto WHERE IdProducto=@Id";
-            Comand.Parameters.AddWithValue("@Id", Id);
-            BaseDeDatos.Conection.Open();
-            SqlDataReader reader = Comand.ExecuteReader();
-            while (reader.Read())
-            {
+            string sql = "SELECT * FROM ImagenesProducto WHERE IdProducto=@IdProducto";
+            var ListaImg = BaseDeDatos.Conection.Query(sql, new { @IdProducto = Id }).ToList();
 
-                byte[] ImagenBytes = (byte[])reader["Imagen"];
-                ListaDeImagenes.Add(ImagenBytes);
+            foreach (var item in ListaImg)
+            {      
+                ListaDeImagenes.Add((byte[])item.Imagen);
             }
-            BaseDeDatos.Conection.Close();
             return ListaDeImagenes;
         }
 

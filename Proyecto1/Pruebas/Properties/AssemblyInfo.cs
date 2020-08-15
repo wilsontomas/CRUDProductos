f5a3ca56a8e05a6db963bb2eb6a14de242150e54
Wilson Tomas <wilsonpumm@gmail.com>
2020-08-15 12:56:32 -0400

    Se agregaron las pruebas

---
+++ Proyecto1/Pruebas/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pruebas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pruebas")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("57ac8de9-8a34-4de6-bbf5-e3924bd72a4d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Proyecto1/Pruebas/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibreriaDeClases;
+using System.Collections.Generic;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        MetodosDeCrud metodos = new MetodosDeCrud();
+        [TestMethod]
+        public void PruebaImagenes()
+        {          
+            var listaEsperada = new List<byte[]>();
+            var listaActual = metodos.ObtenerImagenesPorId(8);
+            Assert.AreEqual(listaEsperada.GetType(), listaActual.GetType());
+        }
+
+        [TestMethod]
+        public void PruebaSuplidores()
+        {          
+            var listaEsperada = new List<Suplidor>();
+            var listaActual = metodos.ObtenerSuplidoresPorId(8);          
+            Assert.AreEqual(listaEsperada.GetType(), listaActual.GetType());        
+        }
+
+        [TestMethod]
+        public void PruebaCategorias()
+        {
+            var listaEsperada = new List<Categoria>();
+            var listaActual = metodos.ObtenerCategorias();
+            Assert.AreEqual(listaEsperada.GetType(), listaActual.GetType());
+        }
+    }
+}
